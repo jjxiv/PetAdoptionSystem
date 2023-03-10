307d8fb2667b97adafdf501c6b622910321b96d5
jrchjhn <almorojericho14@gmail.com>
2023-03-11 02:53:18 +0800

    New files 2:53am 03/11/2023 -jj

--- adminMainMenu.py
+++ adminMainMenu.py
@@ -1,40 +1,7 @@
 import mainMenu
-
-#user login module goes here
-
-def adminMainMenu():
-    print("==========Pet Adoption System==========")
-    print("[1] Add Pets")
-    print("[2] Remove Pets")
-    print("[3] Modify Pet Information")
-    print("[4] Logout")
-
-    userMainMenuSelection = int(input("Enter a number:"))
-
-    match userMainMenuSelection:
-        case 1:  #adminAddPets()
-            pass
-        case 2:  #adminRemovePets()
-            pass
-        case 3:  #adminModifyPets()
-            pass
-        case 4:  #userLogout()
-            # log out the user
-            mainMenu.mainMenu()
-            pass
-        case _:
-            print("Invalid input. Please try again.")
-            adminMainMenu()
-            pass
-
-adminMainMenu()
-
-=======
-import mainMenu
 import adminAddPets
 import adminRemovePets
 import adminModifyPets
-
 
 #user login module goes here
 
@@ -64,4 +31,3 @@ def adminMainMenu():
             print("Invalid input. Please try again.")
             adminMainMenu()
             pass
->>>>>>> 88b571085a7d40829cc4d67dd71a7cbec5f94ddb
--- viewPets.py
+++ viewPets.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mainMenu
 def availablePets():
     print("==========View Available Pets==========")
@@ -20,27 +19,4 @@ def availablePets():
             mainMenu.mainMenu()
             pass
         case __:
-=======
-import mainMenu
-def availablePets():
-    print("==========View Available Pets==========")
-    print("[1] Dogs")
-    print("[2] Cats")
-    print("[3] Cats")
-    x = int(input("Enter number: "))
-
-    match x:
-        case 1:
-            pass
-            print("==========Dogs==========")
-            #WARNING: display dog pets from txt file
-        case 2:
-            pass
-            print("==========Cats==========")
-            # WARNING: display cat pets from txt file
-        case 3:
-            mainMenu.mainMenu()
-            pass
-        case __:
->>>>>>> 88b571085a7d40829cc4d67dd71a7cbec5f94ddb
             pass
