3041dff6d6ca3818a3516f41dc0001409ce1373f
Jericho Almoro <85814630+jjxiv@users.noreply.github.com>
2023-03-10 20:35:10 +0800

    Add files via upload

---
+++ mainMenu.py
@@ -0,0 +1,35 @@
+"""
+    File name:
+    Authors:
+    Description:
+    Date:
+    Email:
+"""
+
+
+
+if __name__ == "__main__":
+    print("==========Pet Adoption System==========")
+    print("[1] Register")
+    print("[2] User Login")
+    print("[3] Admin Login")
+    print("[4] View Available Pets")
+
+    mm = int(input("Enter a number: "))
+    match mm:
+        case 1:
+            pass
+            # Register Function
+        case 2:
+            pass
+            #User Login
+        case 3:
+            pass
+            #Admin Login
+        case 4:
+            pass
+            #View Available Pets
+        case _:
+            pass
+            print("Error, try again.")
+    pass
